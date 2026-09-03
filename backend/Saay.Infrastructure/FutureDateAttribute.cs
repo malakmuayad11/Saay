9256462a831560d740a4b90ca3d7b22503a7e494
malakmuayad11 <malakmuayad15@gmail.com>
2026-09-03 22:12:01 +0300

    Add date/time validation attributes

---
+++ backend/Saay.Infrastructure/FutureDateAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Saay.Infrastructure
+{
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(
+            object? value, ValidationContext validationContext)
+        {
+            var dueDateProperty = validationContext.ObjectType
+            .GetProperty("DueDate");
+
+            if (dueDateProperty != null)
+            {
+                DateOnly dueDate = (DateOnly)dueDateProperty.GetValue(validationContext.ObjectInstance)!;
+
+                if (dueDate < DateOnly.FromDateTime(DateTime.Now))
+                {
+                    return new ValidationResult("Due date must be in the future.");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ backend/Saay.Infrastructure/FutureTimeAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Saay.Infrastructure
+{
+    public class FutureTimeAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(
+            object? value, ValidationContext validationContext)
+        {
+            var dueTime = validationContext.ObjectType.GetProperty("DueTime");
+
+            if (dueTime != null) { 
+                TimeOnly? dueTimeValue = (TimeOnly?)dueTime.GetValue(validationContext.ObjectInstance);
+                if (dueTimeValue.HasValue)
+                {
+                    DateOnly dueDate = (DateOnly)validationContext.ObjectType.GetProperty("DueDate")!.GetValue(validationContext.ObjectInstance)!;
+                    DateTime dueDateTime = dueDate.ToDateTime(dueTimeValue.Value);
+                    if (dueDateTime <= DateTime.Now)
+                    {
+                        return new ValidationResult("Due time must be in the future.");
+                    }
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
