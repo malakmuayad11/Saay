149f9d37e546bfd6cdef40bebd1a1d7042123071
malakmuayad11 <malakmuayad15@gmail.com>
2026-09-15 20:29:12 +0300

    Update CORS for local development

--- backend/Saay.Extensions/ServiceCollectionExtensions.cs
+++ backend/Saay.Extensions/ServiceCollectionExtensions.cs
@@ -92,7 +92,8 @@ namespace Saay.Extensions
                     policy
                         .WithOrigins(
                             "http://127.0.0.1:5500",
-                            "http://localhost:5109"
+                            "http://localhost:5109",
+                            "http://localhost:5173"
                         )
                         .AllowAnyHeader()
                         .AllowAnyMethod();
